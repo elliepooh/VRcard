3e7a77d90a5ce455aaae99cb18f85fde6eb53233
Liza Kobrazova <lizakobrazova@gmail.com>
2017-03-18 15:19:45 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='main'>
+    <router-view name='bground'></router-view>
+    <router-view name='card'></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
+  background-color: #c6e2ff;
+}
+</style>
---
+++ src/components/bgrounds/Line.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='bground'></div>
+</template>
+
+<script>
+  export default {
+    name: 'bgLine',
+  };
+</script>
+
+<style>
+
+</style>
