674768f2cd134dcdeeb2bf39574756a85a7dfd8b
Liza Kobrazova <lizakobrazova@gmail.com>
2017-03-20 20:49:07 +0300

    Use orbit controls

--- src/components/three.js
+++ src/components/three.js
@@ -1,4 +1,6 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import * as THREE from 'three';
+
+const OrbitControls = require('three-orbit-controls')(THREE);
 
 export default class Three {
   constructor({
@@ -6,10 +8,12 @@ export default class Three {
     transparent = false,
     alpha = 1.0,
     color = 0xffffff,
+    cameraPosition = 100,
   }) {
     this.scene = null;
     this.camera = null;
     this.renderer = null;
+    this.controls = null;
 
     this.container = container;
     this.width = this.container.clientWidth;
@@ -18,18 +22,22 @@ export default class Three {
     this.transparent = transparent;
     this.alpha = alpha;
     this.color = color;
+    this.cameraPosition = cameraPosition;
   }
   init() {
-    this.scene = new Scene();
-    this.camera = new PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
     this.camera.lookAt(this.scene.position);
-    this.camera.position.z = 100;
+    this.camera.position.z = this.cameraPosition;
 
-    this.renderer = new WebGLRenderer({ alpha: this.transparent });
+    this.renderer = new THREE.WebGLRenderer({ alpha: this.transparent });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
     this.renderer.setClearColor(this.color, this.alpha);
 
     this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     window.addEventListener('resize', this.onResize.bind(this));
   }
