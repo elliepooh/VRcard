e98d93597ebca456a4964c52aa454943c8b8c06e
Liza Kobrazova <lizakobrazova@gmail.com>
2017-03-21 22:00:48 +0300

    Add nav component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
-<template>
-  <div class='main'>
-    <router-view name='cubic'></router-view>
-  </div>
+<template lang='pug'>
+  .main
+    card-nav
+    router-view
 </template>
 
 <script>
+import CardNav from './components/CardNav';
+
 export default {
   name: 'app',
+  components: {
+    CardNav,
+  },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  overflow: hidden;
+}
 body {
   width: 100vw;
   height: 100vh;
@@ -18,9 +27,6 @@ body {
 .main {
   width: 100%;
   height: 100%;
-}
-* {
-  margin: 0;
-  overflow: hidden;
+  position: relative;
 }
 </style>
---
+++ src/components/CardNav.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+  .nav
+    router-link.nav-link(to='/cactus') Cactus
+    router-link.nav-link(to='/land') Land
+</template>
+
+<script>
+export default {
+  name: 'cardNav',
+};
+</script>
+
+<style>
+.nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+.nav-link {
+  text-decoration: none;
+  color: #fff;
+  margin-right: 1rem;
+}
+</style>
