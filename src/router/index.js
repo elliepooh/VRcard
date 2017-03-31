8cd36b725ec897266854ce0b36d86a3d09302966
Liza Kobrazova <lizakobrazova@gmail.com>
2017-03-31 18:34:48 +0300

    Remove trial components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Cactus from '@/components/Cactus';
 import BusinessCard from '@/components/BusinessCard';
 import Card from '@/components/Card';
 
@@ -12,9 +11,6 @@ export default new Router({
     {
       path: '/businessCard',
       component: BusinessCard,
-    }, {
-      path: '/cactus',
-      component: Cactus,
     }, {
       path: '/card',
       component: Card,
