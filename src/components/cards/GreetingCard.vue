9f91f6e074616cac9c8d853f5f47c4ec9b81144f
Liza Kobrazova <lizakobrazova@gmail.com>
2017-05-16 17:14:18 +0300

    Add letter

--- src/components/cards/GreetingCard.vue
+++ src/components/cards/GreetingCard.vue
@@ -1,7 +1,10 @@
 <template lang='pug'>
   .card
+    .world
+    .letter(@click='openLetter')
     a.nav-btn.btn-prev(@click='prevCard')
     a.nav-btn.btn-next(@click='nextCard')
+    .message(v-if='showMessage')
 </template>
 
 <script>
@@ -10,6 +13,8 @@ import SaturnCard from './greeting/SaturnCard';
 import SheepCard from './greeting/SheepCard';
 import WatermelonCard from './greeting/WatermelonCard';
 import FireflyCard from './greeting/FireflyCard';
+
+import Letter from './greeting/Letter';
 
 export default {
   name: 'greetingCard',
@@ -23,24 +28,39 @@ export default {
         FireflyCard,
       ],
 
+      worldContainer: null,
       index: 0,
       card: null,
+
+      letter: null,
+      showMessage: false,
     };
   },
   mounted() {
-    this.card = new this.cards[this.index](this.$el);
+    this.worldContainer = this.$el.querySelector('.world');
+    this.card = new this.cards[this.index](this.worldContainer);
     this.card.animate();
+
+    const letterContainer = this.$el.querySelector('.letter');
+    this.letter = new Letter(letterContainer);
+    this.letter.animate();
   },
   methods: {
     prevCard() {
       this.index = this.index === 0 ? this.cards.length - 1 : this.index - 1;
-      this.$el.removeChild(this.$el.children[2]);
-      this.card = new this.cards[this.index](this.$el);
+      this.createCard();
     },
     nextCard() {
       this.index = this.index + 1 === this.cards.length ? 0 : this.index + 1;
-      this.$el.removeChild(this.$el.children[2]);
-      this.card = new this.cards[this.index](this.$el);
+      this.createCard();
+    },
+    createCard() {
+      this.worldContainer.innerHTML = '';
+      this.card = new this.cards[this.index](this.worldContainer);
+    },
+    openLetter() {
+      this.letter.open();
+      this.showMessage = !this.showMessage;
     },
   },
   watch: {
@@ -58,6 +78,21 @@ export default {
   width: 100%;
   height: 100%;
   position: relative;
+}
+.world {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+.message-box {
+  position: absolute;
+  top: 20%;
+  left: 20rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: $border-radius * 2;
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 3rem;
 }
 .nav-btn {
   position: absolute;
@@ -78,5 +113,20 @@ export default {
   right: 3rem;
   background: $color-white url('../../assets/icons/arrowNext.svg') 80% center / 80% no-repeat;
 
+}
+.letter {
+  position: absolute;
+  width: 20rem;
+  height: 25rem;
+  bottom: 8rem;
+  left: 8rem;
+}
+.message {
+  width: 40rem;
+  height: 30rem;
+  background-color: #ffffff;
+  position: absolute;
+  top: calc(50% - 15rem);
+  left: calc(50% - 20rem);
 }
 </style>
---
+++ src/components/cards/greeting/Letter.js
@@ -0,0 +1,146 @@
+import * as THREE from 'three';
+import Three from './Three';
+
+/* eslint-disable no-param-reassign */
+export default class Letter {
+  constructor(container) {
+    this.three = new Three({
+      container,
+      transparent: true,
+      alpha: 0,
+      cameraPositionX: 0,
+      cameraPositionY: 0,
+      cameraPositionZ: 10,
+    });
+    this.width = container.clientWidth;
+    this.height = container.clientHeight;
+
+    this.init();
+  }
+  init() {
+    this.three.init();
+
+    this.group = new THREE.Group();
+    this.group.position.y = -2;
+    this.three.scene.add(this.group);
+
+    this.vAngle = 0;
+    this.isOpen = false;
+
+    this.addLights();
+    this.draw();
+
+    this.group.traverse((object) => {
+      if (object instanceof THREE.Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+      }
+    });
+  }
+  addLights() {
+    const light = new THREE.HemisphereLight();
+    this.three.scene.add(light);
+
+    const directLight1 = new THREE.DirectionalLight();
+    directLight1.castShadow = true;
+    directLight1.position.set(-10, 5, 8);
+    this.three.scene.add(directLight1);
+  }
+  draw() {
+    const bodyMaterial = new THREE.MeshStandardMaterial({
+      color: 0x1CB3DD,
+      roughness: 1,
+      shading: THREE.FlatShading,
+    });
+    const bodyGeometry = new THREE.BoxGeometry(8, 6, 2);
+    const body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+    this.group.add(body);
+
+    const upperPartMaterial = new THREE.MeshStandardMaterial({
+      color: 0xF4FCFF,
+      roughness: 1,
+      shading: THREE.FlatShading,
+    });
+    const upperPartShape = new THREE.Shape([
+      new THREE.Vector2(-4, 0),
+      new THREE.Vector2(4, 0),
+      new THREE.Vector2(0, 4),
+    ]);
+    const upperPartGeometry = new THREE.ExtrudeGeometry(upperPartShape, {
+      amount: 1,
+      bevelSegments: 0,
+      bevelSize: 0,
+      bevelThickness: 0,
+    });
+
+    this.upperPart = new THREE.Mesh(upperPartGeometry, upperPartMaterial);
+    this.upperPart.position.set(0, 3, 1);
+    this.upperPart.rotation.z = Math.PI;
+    this.upperPart.name = 'letterPart';
+    this.group.add(this.upperPart);
+
+    const partMaterial = new THREE.MeshStandardMaterial({
+      color: 0xF24682,
+      roughness: 1,
+      shading: THREE.FlatShading,
+    });
+
+    const bottomPartShape = new THREE.Shape([
+      new THREE.Vector2(-4, 0),
+      new THREE.Vector2(4, 0),
+      new THREE.Vector2(0, 3),
+    ]);
+    const bottomPartGeometry = new THREE.ExtrudeGeometry(bottomPartShape, {
+      amount: 0.5,
+      bevelSegments: 0,
+      bevelSize: 0,
+      bevelThickness: 0,
+    });
+    const bottomPart = new THREE.Mesh(bottomPartGeometry, partMaterial);
+    bottomPart.position.set(0, -3, 1);
+    this.group.add(bottomPart);
+
+    const partShape = new THREE.Shape([
+      new THREE.Vector2(-3, 0),
+      new THREE.Vector2(3, 0),
+      new THREE.Vector2(0, 4),
+    ]);
+    const partGeometry = new THREE.ExtrudeGeometry(partShape, {
+      amount: 0.2,
+      bevelSegments: 0,
+      bevelSize: 0,
+      bevelThickness: 0,
+    });
+
+    const rightPart = new THREE.Mesh(partGeometry, partMaterial);
+    rightPart.position.set(-4, 0, 1);
+    rightPart.rotation.z = -Math.PI / 2;
+    this.group.add(rightPart);
+
+    const leftPart = rightPart.clone();
+    leftPart.position.x = -rightPart.position.x;
+    leftPart.rotation.z = Math.PI / 2;
+    this.group.add(leftPart);
+  }
+  open() {
+    this.isOpen = true;
+  }
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+
+    if (this.isOpen) {
+      if (this.upperPart.rotation.x > -3) {
+        this.vAngle += 0.001;
+        this.upperPart.rotation.x -= Math.sin(this.vAngle);
+      } else {
+        this.vAngle = 0;
+        this.isOpen = false;
+      }
+    } else if (this.upperPart.rotation.x < 0) {
+      this.vAngle += 0.001;
+      this.upperPart.rotation.x += Math.sin(this.vAngle);
+    }
+
+    this.three.render();
+  }
+}
