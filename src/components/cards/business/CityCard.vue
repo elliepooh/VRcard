75c623a3addc2823347f70d3a082c0f626b5b14c
Liza Kobrazova <lizakobrazova@gmail.com>
2017-05-19 18:19:53 +0300

    Change social links

--- src/components/cards/business/CityCard.vue
+++ src/components/cards/business/CityCard.vue
@@ -8,60 +8,36 @@
       img#instagram(src='~@/assets/panoramas/socials/instagram.png')
       img#email(src='~@/assets/panoramas/socials/email.png')
 
+      a-mixin#social(class='link' geometry='primitive: circle; radius: 0.06' material='color: #6B849C')
+
     a-entity(geometry='primitive: plane; width: 1.8; height: 2'
-            material='color: #FFF'
-            position='0 1.8 0')
-      a-plane(width='1.8'
-              height='0.9'
-              position='0 0.7 0.01'
-              src='#photo')
-      a-text(value='Alice Storm'
-            color='#4B5154'
-            width='4'
-            position='-0.45 0 0.01')
-      a-plane(width='1.6'
-              height='0.4'
-              position='0 -0.35 0.01')
+            material='color: #FFF' position='0 1.8 0')
+
+      a-plane(width='1.8' height='0.9' position='0 0.7 0.01' src='#photo')
+      a-text(value='Alice Storm' color='#4B5154' width='4' position='-0.45 0 0.01')
+
+      a-plane(width='1.6' height='0.4' position='0 -0.35 0.01')
         a-text(value='Freelance photographer and illustrator. Love eating muffins, creating fabulous things and never get tired of this.'
-              color='#697982'
-              anchor='center'
-              width='1.6')
-      a-plane(width='1.6'
-              height='0.01'
-              position='0 -0.6 0.01'
-              color='#697982'
-              opacity='0.3')
-      a-plane(width='1.6'
-              height='0.2'
-              position='0 -0.8 0')
-        a-circle(radius='0.07'
-                color='#70B2D3'
-                src='#twitter'
-                position='-0.3 0 0.01')
-        a-circle(radius='0.07'
-                color='#70B2D3'
-                src='#facebook'
-                position='-0.1 0 0.01')
-        a-circle(radius='0.07'
-                color='#70B2D3'
-                src='#instagram'
-                position='0.1 0 0.01')
-        a-circle(radius='0.07'
-                color='#70B2D3'
-                src='#email'
-                position='0.3 0 0.01')
+              color='#697982' anchor='center' width='1.6')
+
+      a-plane(width='1.6' height='0.01' position='0 -0.6 0.01'
+              color='#697982' opacity='0.3')
+
+      a-plane(width='1.6' height='0.2' position='0 -0.8 0')
+        a-entity(mixin='social' material='src: #twitter' position='-0.3 0 0.01')
+        a-entity(mixin='social' material='src: #facebook' position='-0.1 0 0.01')
+        a-entity(mixin='social' material='src: #instagram' position='0.1 0 0.01')
+        a-entity(mixin='social' material='src: #email' position='0.3 0 0.01')
 
     a-sky(radius='10' src='#city')
     a-entity(position='0 2 2')
       a-entity(camera look-controls wasd-controls)
+        a-cursor(fuse='true')
 </template>
 
 <script>
-// import robotoFont from '@/assets/fonts/roboto.fnt';
-// import robotoPng from '@/assets/fonts/roboto.png';
-
 export default {
-  name: 'CityCard',
+  name: 'cityCard',
   data() {
     return {};
   },
