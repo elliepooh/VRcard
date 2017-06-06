b61eee7f6cc73ebd5cf51ffdb7bb1f92d86b2134
Liza Kobrazova <lizakobrazova@gmail.com>
2017-06-06 15:38:59 +0300

    Remove color panel

--- src/components/dashboard/BusinessAssets.vue
+++ src/components/dashboard/BusinessAssets.vue
@@ -5,52 +5,11 @@
       .notification(v-if='message' @click='message = ""') {{ message }}
 
     .settings-windows
-      .settings-multiple
-        .settings-window.card-color
-          .settings-item
-            .item-name Choose color
-            .item-line
-              input.item-radio(
-                type='radio'
-                id='item1'
-                value='6DF2A8'
-                v-model='info.color')
-              label.item-color.color-green(for='item1')
-              input.item-radio(type='radio'
-                id='item2'
-                value='FF8F6D'
-                v-model='info.color')
-              label.item-color.color-orange(for='item2')
-              input.item-radio(
-                type='radio'
-                id='item3'
-                value='50E3C2'
-                v-model='info.color')
-              label.item-color.color-blue(for='item3')
-              input.item-radio(
-                type='radio'
-                id='item4'
-                value='BD10E0'
-                v-model='info.color')
-              label.item-color.color-pink(for='item4')
-              input.item-radio(
-                type='radio'
-                id='item5'
-                value='4A4A4A'
-                v-model='info.color')
-              label.item-color.color-gray(for='item5')
-            input.item-input(
-              placeholder='Enter color hex'
-              name='color'
-              type='text'
-              v-model='info.color')
-
-        .settings-window.card-photo
-          .settings-item
-            .item-name Choose photo
-            .item-photo(:style='{ backgroundImage: `url(${info.imageURL})` }')
-            input.item-upload(name='photo' id='photo' type='file' @change='uploadPhoto')
-            label.btn(for='photo') Upload
+      .settings-window.card-photo
+        .item-name Choose photo
+        .item-photo(:style='{ backgroundImage: `url(${info.imageURL})` }')
+        input.item-upload(name='photo' id='photo' type='file' @change='uploadPhoto')
+        label.btn(for='photo') Upload
 
       .settings-window.card-background
         .settings-item
@@ -82,7 +41,6 @@ export default {
   data() {
     return {
       info: {
-        color: '',
         panorama: '',
         imageURL: '',
       },
@@ -99,7 +57,7 @@ export default {
   },
   created() {
     if (!this.cardsRef) {
-      router.push('/');
+      router.push('/dashboard');
     } else {
       this.getCardData();
       this.getPreviews();
@@ -119,20 +77,13 @@ export default {
       });
     },
     updateInfo() {
-      if (this.checkColorType()) {
-        Object.keys(this.info).forEach((key) => {
-          const value = this.info[key];
-          if (value) {
-            this.cardsRef.child(this.cardname).child(key).set(value);
-          }
-        });
-        this.message = 'Card updated!';
-      } else {
-        this.message = 'Invalid color hex type';
-      }
-    },
-    checkColorType() {
-      return /^[0-9A-F]{6}$/i.test(this.info.color);
+      Object.keys(this.info).forEach((key) => {
+        const value = this.info[key];
+        if (value) {
+          this.cardsRef.child(this.cardname).child(key).set(value);
+        }
+      });
+      this.message = 'Card updated!';
     },
     uploadPhoto(event) {
       const file = event.target.files[0];
@@ -171,85 +122,25 @@ export default {
 .settings-window {
   align-items: center;
 }
+.card-photo {
+  flex: 1;
+  margin-right: 5rem;
+  justify-content: space-between;
+}
 .item-name {
   text-align: center;
   margin-bottom: 2rem;
 }
-.settings-multiple {
-  flex: 1;
-  margin-right: 8rem;
-}
-.card-color {
-  margin-bottom: 4rem;
-}
-.item-radio {
-  display: none;
-}
-.item-radio:checked + .item-color::before {
-  opacity: 1;
-}
-.item-color {
-  width: 1.6rem;
-  height: 1.6rem;
-  border-radius: $border-radius;
-  cursor: pointer;
-  position: relative;
-  &::before {
-    content: '';
-    position: absolute;
-    top: -0.4rem;
-    left: -0.4rem;
-    width: 2rem;
-    height: 2rem;
-    border-width: 2px;
-    border-style: solid;
-    border-radius: $border-radius;
-    opacity: 0;
-  }
-}
-.color-green {
-  background-color: #6DF2A8;
-  &::before {
-    border-color: #6DF2A8;
-  }
-}
-.color-orange {
-  background-color: #FF8F6D;
-  &::before {
-    border-color: #FF8F6D;
-  }
-}
-.color-blue {
-  background-color: #50E3C2;
-  &::before {
-    border-color: #50E3C2;
-  }
-}
-.color-pink {
-  background-color: #BD10E0;
-  &::before {
-    border-color: #BD10E0;
-  }
-}
-.color-gray {
-  background-color: #4A4A4A;
-  &::before {
-    border-color: #4A4A4A;
-  }
-}
-.item-input {
-  text-align: center;
-  padding-bottom: 0.5rem;
-  color: $color-text;
-}
 .item-photo {
-  min-width: 10rem;
-  min-height: 10rem;
+  min-width: 100%;
+  min-height: 20rem;
   background-color: $color-darkgray;
-  margin: 0 auto;
   background-size: cover;
   background-position: center;
   border-radius: $border-radius;
+}
+.card-background {
+  flex: 2;
 }
 .item-upload {
   width: 0.1px;
