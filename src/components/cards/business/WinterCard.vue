253eaabbf3bbf988f13726cb462760a2ae8cc20d
Liza Kobrazova <lizakobrazova@gmail.com>
2017-06-08 18:59:27 +0300

    Remove card transparency

--- src/components/cards/business/WinterCard.vue
+++ src/components/cards/business/WinterCard.vue
@@ -2,7 +2,6 @@
   a-scene
     a-assets
       img#winter(src='~@/assets/panoramas/winter.jpg')
-      img#photo(src='~@/assets/panoramas/owl.jpg')
       img#twitter(src='~@/assets/panoramas/socials/twitter.png')
       img#facebook(src='~@/assets/panoramas/socials/facebook.png')
       img#instagram(src='~@/assets/panoramas/socials/instagram.png')
@@ -12,7 +11,7 @@
                     material='transparent: true')
 
     a-entity(geometry='primitive: plane; width: 3; height: 1.6' position='0 2 0'
-            material='transparent: true; opacity: 0.7; color: #252D4C; side: double')
+            material='color: #252D4C; side: double')
 
       a-text(value='LESTER WAY' color='#F7F5EA' width='4' position='-0.55 0.5 0.01'
             letter-spacing='2' font='exo2semibold')
@@ -21,7 +20,7 @@
   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\
   Ut enim ad minim veniam, quis nostrud exercitation ullamco.'
             color='#F7F5EA' anchor='center' width='2' align='center'
-            letter-spacing='2' line-height='50' position='0 0 0.02')
+            letter-spacing='2' line-height='50' position='0 0 0.01')
 
       a-plane(width='0.5' height='0.01' position='0 -0.4 0.01'
               color='#F7F5EA' opacity='0.3')
@@ -38,13 +37,7 @@
 </template>
 
 <script>
-import 'aframe-gradient-sky';
-
 export default {
   name: 'winterCard',
-  data() {
-    return {};
-  },
-  methods: {},
 };
 </script>
